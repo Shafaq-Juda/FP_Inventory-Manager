5bd74fde9ffcc9df530bcdcfb8bdd62f5d00c6d0
Shafaq-Juda <sjuda008@gmail.com>
2024-04-03 10:41:21 -0500

    Added owner_id column to vendors

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -7,9 +7,12 @@
 #  name         :string
 #  created_at   :datetime         not null
 #  updated_at   :datetime         not null
+#  owner_id     :integer
 #
 class Vendor < ApplicationRecord
+  belongs_to :owner, class_name: "User"
   has_many :own_products, class_name: "Product", foreign_key: "vendor_id"
 
   validates :name, presence: true
+  validates :owner, presence: true  # Ensure that the owner is present
 end
