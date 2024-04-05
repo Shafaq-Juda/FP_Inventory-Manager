711d6de339b230ba7eff00eb434c96e050dd991e
Shafaq-Juda <sjuda008@gmail.com>
2024-04-05 12:35:37 -0500

    Created sold column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_04_213301) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_04_220327) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_04_213301) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "vendor_name"
+    t.boolean "sold"
     t.index ["owner_id"], name: "index_products_on_owner_id"
   end
 
